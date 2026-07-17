78ea555cd4fdb27e40ac323f4bebf48b96ad6b4a
kitsanaphongthongsuk-collab <kitsanaphong.thongsuk@e-tech.ac.th>
2026-07-17 11:48:21 +0700

    Add files via upload

---
+++ edit_game.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $id = $_GET['id'];
+
+        include 'action/connect.php';
+
+        $sql = "SELECT * FROM games WHERE game_id = '$id' ";
+
+        $result = mysqli_query($con, $sql);
+
+        $game = mysqli_fetch_assoc($result);
+
+        var_dump($game);
+?>
+    
+    <form action="action/insert_game.php" method="post">
+
+            <label for="">รหัสเกม</label>
+            <input type="text" name="game_id"> <br>
+
+            <label for="">ชื่อเกม</label>
+            <input type="text" name="game_name"> <br>
+
+            <label for="">ราคา</label>
+            <input type="text" name="game_price"> <br>
+
+            <label for="">ลิ้งก์ภาพปก</label>
+            <input type="text" name="game_cover"> <br>
+
+            <?php
+                include 'action/connect.php';
+
+                $sql = "SELECT * FROM game_types";
+        
+                $result = mysqli_query($con, $sql);
+            ?>
+
+            <label for="">ประเภท</label>
+            <select name="type_id" id="">
+                <?php
+                        foreach($result as $type){
+                            ?>
+                                <option value="<?= $type["type_id"] ?> "> <?= $type["type_name"] ?> </option>
+                            <?php
+                        }
+                ?>
+            </select>
+
+            <br>
+            <button>บันทึก</button>
+
+    </form>
+
+</body>
+</html>
